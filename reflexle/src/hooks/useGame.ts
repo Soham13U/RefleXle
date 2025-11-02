114875bf6773c5d929fe178164c04e876819da15
Soham13U <sohamup13@gmail.com>
2025-11-02 17:00:33 +0530

    Deploy fix

--- reflexle/src/hooks/useGame.ts
+++ reflexle/src/hooks/useGame.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { initPRNG, makeDailySeed, makeRandomSeed, pickHiddenWord, scheduleFixedGuesses } from "../logic/seed";
 import { computeWordleFeedback } from "../logic/feedback";
 import { comparePatterns } from "../logic/patterns";
